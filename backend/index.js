e8b7ea355158062b83978a6368243cd1eb5ad96c
Tobi Adeleke <tobiaadeleke@gmail.com>
2023-12-26 23:22:57 +0000

    add middlewares to server

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
-
+import cors from 'cors';
+import "dotenv/config";
 
 const app = express();
-
+app.use(express.json()); // allows automatic conversion to json
+app.use(express.urlencoded({extended:true})); // Helps parse url, to get i.e query parameters
+app.use(cors()); // A security set to  prevent request from differnt URL that's unwanted
 const PORT = 3000;
 
 app.listen(PORT, () =>{
