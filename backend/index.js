5c5bed88b6b35c959f13575920c90f865b16987d
Tobi Adeleke <tobiaadeleke@gmail.com>
2024-01-01 23:04:23 +0000

    add deployment script

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,15 @@ import dotenv from 'dotenv';
 import authRouter from './routes/auth.js';
 // import userRouter from './routes/users.js';
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config({path: process.env.DOTENV_CONFIG_PATH});
 
 mongoose.connect(process.env.DATABASE_URL).then(()=>{
     console.log("Connected to DB");
 });
+
+const __dirname = path.resolve();
 
 const PORT = 3000;
 const app = express();
@@ -26,6 +29,11 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
 // app.use("/api/users", userRouter);
+
+app.use(express.static(path.join(__dirname, '/frontend/dist' )));
+app.get("*", (req, res) =>{
+    res.sendFile(path.join(__dirname, 'frontend','dist', 'index.html'));
+})
 
 
 app.listen(PORT, () =>{
